52ace7cbfb165820df890cd6065b27988cfc0ca4
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2019-05-22 20:07:12 -0400

    Fix behavior with Markdown 3.0

--- figureAltCaption.py
+++ figureAltCaption.py
@@ -30,64 +30,70 @@ would generate a figure like this:
 
 from __future__ import unicode_literals
 from markdown import Extension
-from markdown.inlinepatterns import IMAGE_LINK_RE, IMAGE_REFERENCE_RE
+# from markdown.inlinepatterns import IMAGE_LINK_RE, IMAGE_REFERENCE_RE
 from markdown.blockprocessors import BlockProcessor
 from markdown.util import etree
-import re #regex
+from markdown import util as md_util
+import re  # regex
 
 import logging
 logger = logging.getLogger('MARKDOWN')
 
-FIGURES = [u'^\s*'+IMAGE_LINK_RE, u'^\s*'+IMAGE_REFERENCE_RE] #is: linestart, any whitespace (even none), image, any whitespace (even none), line ends.
-CAPTION = r'\[(?P<caption>[^\]]*)\]' # Get the contents within the first set of brackets
-ATTR = r'\{(?P<attributes>[^\}]*)\}'
+
+NOBRACKET = r'[^\]\[]*'
+BRK = (
+    r'\[(' +
+    (NOBRACKET + r'(\[') * 6 +
+    (NOBRACKET + r'\])*') * 6 +
+    NOBRACKET + r')\]'
+)
+IMAGE_LINK_RE = r'\!' + BRK + r'\s*\(\s*(<.*?>|([^"\)\s]+\s*"[^"]*"|[^\)\s]*))\s*\)'
+IMAGE_REFERENCE_RE = r'\!' + BRK + r'\s?\[([^\]]*)\]'
+
+# is: linestart, any whitespace (even none), image, any whitespace (even none), line ends.
+FIGURES = [u'^\s*' + IMAGE_LINK_RE + u'\s*$',
+           u'^\s*' + IMAGE_REFERENCE_RE + u'\s*$']
+
 
 # This is the core part of the extension
-class FigureCaptionProcessor(BlockProcessor):
-    FIGURES_RE = re.compile('|'.join(f for f in FIGURES)) # Identifies the figures
-    CAPTION_RE = re.compile(CAPTION) # Identifies the figure caption
-    ATTR_RE = re.compile(ATTR) # Identifies the figure caption
+class OldFigureCaptionProcessor(BlockProcessor):
+    FIGURES_RE = re.compile('|'.join(f for f in FIGURES))
 
-    def test(self, parent, block): # is the block relevant
-        # Wenn es ein Bild gibt und das Bild alleine im paragraph ist, und das Bild nicht schon einen figure parent hat, returne True
+    def test(self, parent, block):  # is the block relevant
+        # If there is an image and the image is alone in the paragraph, and the image doesn't already have a figure parent, return True
         isImage = bool(self.FIGURES_RE.search(block))
-        isOnlyOneLine = (len(block.splitlines())== 1)
+        isOnlyOneLine = (len(block.splitlines()) == 1)
         isInFigure = (parent.tag == 'figure')
-        
+
         # print(block, isImage, isOnlyOneLine, isInFigure, "T,T,F")
         if (isImage and isOnlyOneLine and not isInFigure):
-            print(block)
             return True
         else:
             return False
 
-    def run(self, parent, blocks): # how to process the block?
+    def run(self, parent, blocks):  # how to process the block?
         raw_block = blocks.pop(0)
-        captionText = self.CAPTION_RE.search(raw_block).group('caption')
-        try:
-            attrText = self.ATTR_RE.search(raw_block).group('attributes') # Get the caption text
-        except:
-            attrText = None
+        captionText = self.FIGURES_RE.search(raw_block).group(1)
 
         # create figure
         figure = etree.SubElement(parent, 'figure')
-
-        if attrText:
-            figure.set('id',attrText)
 
         # render image in figure
         figure.text = raw_block
 
         # create caption
-        figcaptionElem = etree.SubElement(figure,'figcaption')
-        figcaptionElem.text = captionText #no clue why the text itself turns out as html again and not raw. Anyhow, it suits me, the blockparsers annoyingly wrapped everything into <p>.
+        figcaptionElem = etree.SubElement(figure, 'figcaption')
+        # no clue why the text itself turns out as html again and not raw. Anyhow, it suits me, the blockparsers annoyingly wrapped everything into <p>.
+        figcaptionElem.text = captionText
+
 
 class FigureCaptionExtension(Extension):
     def extendMarkdown(self, md, md_globals):
         """ Add an instance of FigcaptionProcessor to BlockParser. """
-        md.parser.blockprocessors.add('figureAltcaption',
-                                      FigureCaptionProcessor(md.parser),
-                                      '<ulist')
+        md.parser.blockprocessors.register(OldFigureCaptionProcessor(md.parser), 'figureAltcaption', 200)
+        # md.inlinePatterns.register(FigureCaptionProcessor(IMAGE_LINK_RE, md), 'figureAltcaption', 190)
+        md.registerExtension(self)
+
 
 def makeExtension(**kwargs):
     return FigureCaptionExtension(**kwargs)
